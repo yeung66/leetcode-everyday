f23e5e3ffaba064592ed695817041bcfe3fe6964
Scott Yeung <407284292@qq.com>
2021-03-05 09:08:33 +0800

    solve 2 problems at 03/05/2021 09:08:32

---
+++ js/543.二叉树的直径.js
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=543 lang=javascript
+ *
+ * [543] 二叉树的直径
+ *
+ * https://leetcode-cn.com/problems/diameter-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (52.21%)
+ * Likes:    626
+ * Dislikes: 0
+ * Total Accepted:    95.2K
+ * Total Submissions: 182.4K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+ * 
+ * 
+ * 
+ * 示例 :
+ * 给定二叉树
+ * 
+ * ⁠         1
+ * ⁠        / \
+ * ⁠       2   3
+ * ⁠      / \     
+ * ⁠     4   5    
+ * 
+ * 
+ * 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+ * 
+ * 
+ * 
+ * 注意：两结点之间的路径长度是以它们之间边的数目表示。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+    let ans = 1
+    const dfs = (node) => {
+        if(node===null) return 0
+        const left = dfs(node.left)
+        const right = dfs(node.right)
+        ans = Math.max(left+right+1, ans)
+
+        return Math.max(left,right)+1
+    }
+
+    dfs(root)
+    return ans - 1
+};
+
+
+// @lc code=end
+
---
+++ js/57.插入区间.js
@@ -0,0 +1,107 @@
+/*
+ * @lc app=leetcode.cn id=57 lang=javascript
+ *
+ * [57] 插入区间
+ *
+ * https://leetcode-cn.com/problems/insert-interval/description/
+ *
+ * algorithms
+ * Medium (40.32%)
+ * Likes:    373
+ * Dislikes: 0
+ * Total Accepted:    64.1K
+ * Total Submissions: 158.9K
+ * Testcase Example:  '[[1,3],[6,9]]\n[2,5]'
+ *
+ * 给你一个 无重叠的 ，按照区间起始端点排序的区间列表。
+ * 
+ * 在列表中插入一个新的区间，你需要确保列表中的区间仍然有序且不重叠（如果有必要的话，可以合并区间）。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：intervals = [[1,3],[6,9]], newInterval = [2,5]
+ * 输出：[[1,5],[6,9]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+ * 输出：[[1,2],[3,10],[12,16]]
+ * 解释：这是因为新的区间 [4,8] 与 [3,5],[6,7],[8,10] 重叠。
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：intervals = [], newInterval = [5,7]
+ * 输出：[[5,7]]
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：intervals = [[1,5]], newInterval = [2,3]
+ * 输出：[[1,5]]
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：intervals = [[1,5]], newInterval = [2,7]
+ * 输出：[[1,7]]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * intervals[i].length == 2
+ * 0 
+ * intervals 根据 intervals[i][0] 按 升序 排列
+ * newInterval.length == 2
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+    const n = intervals.length
+    let i = 0
+    const ans = []
+
+    while(i < n && intervals[i][1] < newInterval[0]) {
+        ans.push(intervals[i])
+        i++
+    }
+
+    while(i < n && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(intervals[i][0], newInterval[0])
+        newInterval[1] = Math.max(intervals[i][1], newInterval[1])
+        i++
+    }
+
+    ans.push(newInterval)
+
+    while(i < n) {
+        ans.push(intervals[i])
+        i++
+    }
+
+    return ans
+
+
+
+};
+// @lc code=end
+

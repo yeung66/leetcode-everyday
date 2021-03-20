79e3967bcaecb004c23424bfba3e1bd8e84f0ace
Scott Yeung <407284292@qq.com>
2021-03-20 12:35:36 +0800

    solve 3 problems at 03/20/2021 12:35:36

---
+++ js/654.最大二叉树.js
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=654 lang=javascript
+ *
+ * [654] 最大二叉树
+ *
+ * https://leetcode-cn.com/problems/maximum-binary-tree/description/
+ *
+ * algorithms
+ * Medium (81.43%)
+ * Likes:    257
+ * Dislikes: 0
+ * Total Accepted:    37K
+ * Total Submissions: 45.4K
+ * Testcase Example:  '[3,2,1,6,0,5]'
+ *
+ * 给定一个不含重复元素的整数数组 nums 。一个以此数组直接递归构建的 最大二叉树 定义如下：
+ * 
+ * 
+ * 二叉树的根是数组 nums 中的最大元素。
+ * 左子树是通过数组中 最大值左边部分 递归构造出的最大二叉树。
+ * 右子树是通过数组中 最大值右边部分 递归构造出的最大二叉树。
+ * 
+ * 
+ * 返回有给定数组 nums 构建的 最大二叉树 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [3,2,1,6,0,5]
+ * 输出：[6,3,5,null,2,0,null,null,1]
+ * 解释：递归调用如下所示：
+ * - [3,2,1,6,0,5] 中的最大值是 6 ，左边部分是 [3,2,1] ，右边部分是 [0,5] 。
+ * ⁠   - [3,2,1] 中的最大值是 3 ，左边部分是 [] ，右边部分是 [2,1] 。
+ * ⁠       - 空数组，无子节点。
+ * ⁠       - [2,1] 中的最大值是 2 ，左边部分是 [] ，右边部分是 [1] 。
+ * ⁠           - 空数组，无子节点。
+ * ⁠           - 只有一个元素，所以子节点是一个值为 1 的节点。
+ * ⁠   - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
+ * ⁠       - 只有一个元素，所以子节点是一个值为 0 的节点。
+ * ⁠       - 空数组，无子节点。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [3,2,1]
+ * 输出：[3,null,2,null,1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 0 
+ * nums 中的所有整数 互不相同
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var constructMaximumBinaryTree = function(nums) {
+    if(nums.length === 0) return null
+
+    const maxVal = Math.max(...nums)
+    let maxIdx = nums.findIndex((val)=>val===maxVal)
+
+    let root = new TreeNode(maxVal)
+    root.left = constructMaximumBinaryTree(nums.slice(0, maxIdx))
+    root.right = constructMaximumBinaryTree(nums.slice(maxIdx+1))
+
+    return root
+};
+// @lc code=end
+

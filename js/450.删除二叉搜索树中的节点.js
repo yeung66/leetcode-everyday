8065f812484145646e7f6564ea8b5394cf9837e7
Scott Yeung <407284292@qq.com>
2021-03-23 11:47:18 +0800

    solve 4 problems at 03/23/2021 11:47:18

---
+++ js/450.删除二叉搜索树中的节点.js
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode.cn id=450 lang=javascript
+ *
+ * [450] 删除二叉搜索树中的节点
+ *
+ * https://leetcode-cn.com/problems/delete-node-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (46.45%)
+ * Likes:    416
+ * Dislikes: 0
+ * Total Accepted:    36.7K
+ * Total Submissions: 78.8K
+ * Testcase Example:  '[5,3,6,2,4,null,7]\n3'
+ *
+ * 给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key
+ * 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的根节点的引用。
+ * 
+ * 一般来说，删除节点可分为两个步骤：
+ * 
+ * 
+ * 首先找到需要删除的节点；
+ * 如果找到了，删除它。
+ * 
+ * 
+ * 说明： 要求算法时间复杂度为 O(h)，h 为树的高度。
+ * 
+ * 示例:
+ * 
+ * 
+ * root = [5,3,6,2,4,null,7]
+ * key = 3
+ * 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 3   6
+ * ⁠/ \   \
+ * 2   4   7
+ * 
+ * 给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+ * 
+ * 一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+ * 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 4   6
+ * ⁠/     \
+ * 2       7
+ * 
+ * 另一个正确答案是 [5,2,6,null,4,null,7]。
+ * 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 2   6
+ * ⁠  \   \
+ * ⁠   4   7
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+const getMin = (node) => {
+    while(node.left!=null) node = node.left
+    return node
+}
+
+var deleteNode = function(root, key) {
+    
+    if(root==null) return null
+    if(root.val===key) {
+        if(root.left==null) return root.right
+        if(root.right==null) return root.left
+
+        let min = getMin(root.right)
+        root.val = min.val
+        root.right = deleteNode(root.right, min.val)
+    } else if (root.val > key) {
+        root.left = deleteNode(root.left, key)
+    } else {
+        root.right = deleteNode(root.right, key)
+    }
+
+    return root
+};
+// @lc code=end
+

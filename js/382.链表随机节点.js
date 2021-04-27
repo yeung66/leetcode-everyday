f0dfe93eee6424f8ab9b27b159d1c30457eb5f4c
Scott Yeung <407284292@qq.com>
2021-04-27 11:16:23 +0800

    solve 3 problems at 04/27/2021 11:16:23

---
+++ js/382.链表随机节点.js
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode.cn id=382 lang=javascript
+ *
+ * [382] 链表随机节点
+ *
+ * https://leetcode-cn.com/problems/linked-list-random-node/description/
+ *
+ * algorithms
+ * Medium (60.42%)
+ * Likes:    128
+ * Dislikes: 0
+ * Total Accepted:    11.9K
+ * Total Submissions: 19.6K
+ * Testcase Example:  '["Solution","getRandom","getRandom","getRandom","getRandom","getRandom"]\n' +
+  '[[[1,2,3]],[],[],[],[],[]]'
+ *
+ * 给定一个单链表，随机选择链表的一个节点，并返回相应的节点值。保证每个节点被选的概率一样。
+ * 
+ * 进阶:
+ * 如果链表十分大且长度未知，如何解决这个问题？你能否使用常数级空间复杂度实现？
+ * 
+ * 示例:
+ * 
+ * 
+ * // 初始化一个单链表 [1,2,3].
+ * ListNode head = new ListNode(1);
+ * head.next = new ListNode(2);
+ * head.next.next = new ListNode(3);
+ * Solution solution = new Solution(head);
+ * 
+ * // getRandom()方法应随机返回1,2,3中的一个，保证每个元素被返回的概率相等。
+ * solution.getRandom();
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node.
+ * @param {ListNode} head
+ */
+var Solution = function(head) {
+       this.head = head
+};
+
+/**
+ * Returns a random node's value.
+ * @return {number}
+ */
+Solution.prototype.getRandom = function() {
+       let count=0, reserve
+       let head = this.head
+       while(head) {
+              count++
+              if(Math.floor(Math.random()*count)+1===count) {
+                     reserve = head
+              }
+              head = head.next
+       }
+       return reserve.val
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(head)
+ * var param_1 = obj.getRandom()
+ */
+// @lc code=end
+

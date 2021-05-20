72ebd4a8cdb8731d6da1b39dc6081cc65e881b20
Scott Yeung <407284292@qq.com>
2021-05-20 20:58:44 +0800

    solve 3 problems at 05/20/2021 20:58:44

--- scripts/classify.ps1
+++ scripts/classify.ps1
@@ -7,6 +7,11 @@ foreach($f in $files){
         $ex = $ex.Substring(1)
         if(!(Test-Path $ex)) {New-Item $ex -ItemType Directory}
         $name = $f.Name
+        
+        if(Test-Path "$ex\$name") {
+            $basename = $f.BaseName
+            $name = "$basename-1.$ex"
+        }
         Move-Item $f -Destination "$ex\$name"
     }
 }

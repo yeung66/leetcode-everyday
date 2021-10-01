77a35a1b44f51621ba2764653a4c4da43f12ddc1
YeungYeah <yeungyeah@qq.com>
2021-10-01 13:42:48 +0800

    solve 2 problems at 10/01/2021 13:42:48

---
+++ java/97.交错字符串.java
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=97 lang=java
+ *
+ * [97] 交错字符串
+ *
+ * https://leetcode-cn.com/problems/interleaving-string/description/
+ *
+ * algorithms
+ * Medium (45.70%)
+ * Likes:    551
+ * Dislikes: 0
+ * Total Accepted:    61.5K
+ * Total Submissions: 134.9K
+ * Testcase Example:  '"aabcc"\n"dbbca"\n"aadbbcbcac"'
+ *
+ * 给定三个字符串 s1、s2、s3，请你帮忙验证 s3 是否是由 s1 和 s2 交错 组成的。
+ * 
+ * 两个字符串 s 和 t 交错 的定义与过程如下，其中每个字符串都会被分割成若干 非空 子字符串：
+ * 
+ * 
+ * s = s1 + s2 + ... + sn
+ * t = t1 + t2 + ... + tm
+ * |n - m| 
+ * 交错 是 s1 + t1 + s2 + t2 + s3 + t3 + ... 或者 t1 + s1 + t2 + s2 + t3 + s3 +
+ * ...
+ * 
+ * 
+ * 提示：a + b 意味着字符串 a 和 b 连接。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+ * 输出：false
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：s1 = "", s2 = "", s3 = ""
+ * 输出：true
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * 0 
+ * s1、s2、和 s3 都由小写英文字母组成
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int n = s1.length(), m = s2.length(), t = s3.length();
+
+        if (n + m != t) {
+            return false;
+        }
+
+        boolean[] f = new boolean[m + 1];
+
+        f[0] = true;
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= m; ++j) {
+                int p = i + j - 1;
+                if (i > 0) {
+                    f[j] = f[j] && s1.charAt(i - 1) == s3.charAt(p);
+                }
+                if (j > 0) {
+                    f[j] = f[j] || (f[j - 1] && s2.charAt(j - 1) == s3.charAt(p));
+                }
+            }
+        }
+
+        return f[m];
+    }
+}
+// @lc code=end
+

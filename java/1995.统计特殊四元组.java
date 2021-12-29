f47b8e9502465a121ca7c513c4e4a2ee282cc4ac
YeungYeah <yeungyeah@qq.com>
2021-12-29 21:59:24 +0800

    solve 5 problems at 12/29/2021 21:59:24

---
+++ java/1995.统计特殊四元组.java
@@ -0,0 +1,83 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=1995 lang=java
+ *
+ * [1995] 统计特殊四元组
+ *
+ * https://leetcode-cn.com/problems/count-special-quadruplets/description/
+ *
+ * algorithms
+ * Easy (54.57%)
+ * Likes:    83
+ * Dislikes: 0
+ * Total Accepted:    27.6K
+ * Total Submissions: 42.1K
+ * Testcase Example:  '[1,2,3,6]'
+ *
+ * 给你一个 下标从 0 开始 的整数数组 nums ，返回满足下述条件的 不同 四元组 (a, b, c, d) 的 数目 ：
+ * 
+ * 
+ * nums[a] + nums[b] + nums[c] == nums[d] ，且
+ * a < b < c < d
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：nums = [1,2,3,6]
+ * 输出：1
+ * 解释：满足要求的唯一一个四元组是 (0, 1, 2, 3) 因为 1 + 2 + 3 == 6 。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：nums = [3,3,6,4,5]
+ * 输出：0
+ * 解释：[3,3,6,4,5] 中不存在满足要求的四元组。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：nums = [1,1,1,3,5]
+ * 输出：4
+ * 解释：满足要求的 4 个四元组如下：
+ * - (0, 1, 2, 3): 1 + 1 + 1 == 3
+ * - (0, 1, 3, 4): 1 + 1 + 3 == 5
+ * - (0, 2, 3, 4): 1 + 1 + 3 == 5
+ * - (1, 2, 3, 4): 1 + 1 + 3 == 5
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 4 <= nums.length <= 50
+ * 1 <= nums[i] <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int countQuadruplets(int[] nums) {
+        int ans = 0, n = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    for (int l = k + 1; l < n; l++) {
+                        if (nums[i] + nums[j] + nums[k] == nums[l]) {
+                            ans++;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
